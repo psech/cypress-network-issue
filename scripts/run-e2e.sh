b0745a93f25c8a93f6979615235e183eb5c5a9ce
Przemek Sech <przemek.sech@domain.com.au>
2023-05-24 16:19:30 +1000

    test: ping app server

--- scripts/run-e2e.sh
+++ scripts/run-e2e.sh
@@ -1,6 +1,7 @@
 echo "Starting e2e script"
 
 echo DEBUG: Test network connection
+ping -c 4 server.local
 curl -s -o /dev/null -w "%{json}" http://server.local:3000 | jq
 
 # yarn run test:e2e-cypress-experiment-chrome
